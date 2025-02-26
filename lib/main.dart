eba2c1a4cf2d84a62ad0973fbeb6196019de7e9b
timaZapadniy <zapadniysoft@gmail.com>
2025-02-26 20:56:09 +0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:gamepads/gamepads.dart';
+
+void main() {
+  runApp(SmartHomeApp());
+}
+
+class SmartHomeApp extends StatefulWidget {
+  @override
+  _SmartHomeAppState createState() => _SmartHomeAppState();
+}
+
+class _SmartHomeAppState extends State<SmartHomeApp> {
+  String lastButtonPressed = "";
+
+  @override
+  void initState() {
+    super.initState();
+    Gamepads.events.listen((event) {
+      setState(() {
+        lastButtonPressed = event.toString();
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Text(
+            """
+  +------------------+
+  |  SMART HOME UI  |
+  +------------------+
+  |  Lights: OFF    |
+  |  Temp: 22°C     |
+  |  Door: Locked   |
+  +------------------+
+
+  Last Button Pressed: $lastButtonPressed
+  """,
+            style: TextStyle(
+                fontFamily: 'Courier', color: Colors.green, fontSize: 16),
+          ),
+        ),
+      ),
+    );
+  }
+}
